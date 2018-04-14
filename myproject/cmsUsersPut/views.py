f5258aa58b773ebcc082960336c77a6183bfb2a5
apayol <apayolmontero@gmail.com>
2018-04-14 21:20:24 +0200

    Añadida función de loggeo

--- myproject/cmsUsersPut/views.py
+++ myproject/cmsUsersPut/views.py
@@ -4,6 +4,16 @@ from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
+def logg(request):
+    if request.user.is_authenticated():
+        logged = "> Logged in as " + request.user.username
+        logged += ". <a href='/logout'> Logout </a><br>"
+    else:
+        logged = "> Not logged in. "
+        logged += "<a href='/login'> Login </a><br>"
+    return logged
+
+
 def inicio(request):
     if request.method == "GET":
         titulo = "<h3>Bienvenido a cmsUsersPut</h3>"
@@ -12,29 +22,24 @@ def inicio(request):
         for pagina in paginas:
             respuesta += "<ul><li>" + pagina.name + " => "
             respuesta += pagina.page + "</ul></li>"
-
-        #  Logged:
-        if request.user.is_authenticated():
-            logged = "> Logged in as " + request.user.username
-            logged += ". <a href='/logout'> Logout </a><br>"
-        else:
-            logged = "> Not logged in. "
-            logged += "<a href='/login'> Login </a><br>"
+        logged = logg(request)
     else:
         respuesta = "Método no permitido"
     return HttpResponse(titulo + logged + respuesta)
 
 
 def pagina(request, name):
+    logged = logg(request)
+
     if request.method == "GET":
         try:
             pagina = Pages.objects.get(name=name)
-            respuesta = pagina.page
+            respuesta = "<br>" + pagina.page
         except Pages.DoesNotExist:
             return HttpResponseNotFound("La página no existe")
     else:
         respuesta = "Método no permitido"
-    return HttpResponse(respuesta)
+    return HttpResponse(logged + respuesta)
 
 def login_exito (request):
     respuesta = "Logged in as " + request.user.username
